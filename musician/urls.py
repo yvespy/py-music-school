36aeff09c7a8f1e9bd4092cd141b941bf743efa9
yevpy <eugene2piskun@gmail.com>
2025-04-21 18:35:31 +0300

    fixed urls

--- musician/urls.py
+++ musician/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
 from musician.views import MusicianViewSet
 
-router = DefaultRouter()
-router.register(r"", MusicianViewSet)
+router = routers.DefaultRouter()
+
+router.register("musicians", MusicianViewSet, basename="manage")
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include(router.urls))
 ]
 
 app_name = "musician"
